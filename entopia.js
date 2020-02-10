e108b045cda95f58f6fc0b98f890ba44d3369c52
chinwailun <33524499+chinwailun@users.noreply.github.com>
2020-02-10 15:24:27 +0800

    dknd

--- entopia.js
+++ entopia.js
@@ -4,6 +4,8 @@ const config = require('./config');
 const pg = require('pg');
 pg.defaults.ssl = true;
 
+
+
 module.exports = {
     //this function read all the opinions from database
     readAllOpinions: function(callback) {
