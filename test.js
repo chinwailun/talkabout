953bd3bce3cb4993c16151ab8c49744f5210b38a
chinwailun <33524499+chinwailun@users.noreply.github.com>
2020-04-24 15:07:09 +0800

    'd

---
+++ test.js
@@ -0,0 +1,16 @@
+/*const Grammarbot = require('grammarbot');
+ 
+const bot = new Grammarbot({
+  'api_key' : 'node_default',      // (Optional) defaults to node_default
+  'language': 'en-US',         // (Optional) defaults to en-US
+  'base_uri': 'api.grammarbot.io', // (Optional) defaults to api.grammarbot.io
+});
+ 
+// Callback style
+bot.check("I has a book", function(error, result) {
+  if (!error) console.log(JSON.stringify(result));
+});
+ 
+// Async/Await style
+const result = bot.checkAsync("I has a book.");
+console.log(JSON.stringify(result));*/
